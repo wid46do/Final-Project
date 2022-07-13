7eddf11d503ec06dc1b904015e8174bddbbf900c
riorasyid23 <rio.rasyid23@gmail.com>
2022-07-13 21:15:37 +0700

    update rio

--- src/components/Homemain.jsx
+++ src/components/Homemain.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import Homecard from "./Homecard";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import Slider from "react-slick";
@@ -33,7 +33,7 @@ export default function Homemain(){
         variableWidth: true,
         responsive: [
             {
-                breakpoint: 400,
+                breakpoint: 450,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3,
@@ -143,51 +143,6 @@ export default function Homemain(){
                     </div>
                 </Slider>
 
-
-                {/* <div className="home-category">
-                    <div>
-                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M17.5 17.5L13.875 13.875" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                        </svg>
-                        Semua
-                    </div>
-                    <div>
-                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M17.5 17.5L13.875 13.875" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                        </svg> 
-                        Hobi
-                    </div>
-                    <div>
-                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M17.5 17.5L13.875 13.875" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                        </svg>
-                        Kendaraan
-                    </div>
-                    <div>
-                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M17.5 17.5L13.875 13.875" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                        </svg>
-                        Baju
-                    </div>
-                    <div>
-                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M17.5 17.5L13.875 13.875" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                        </svg>
-                        Elektronik
-                    </div>
-                    <div>
-                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M17.5 17.5L13.875 13.875" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                        </svg>                    
-                        Kesehatan
-                    </div>
-                </div> */}
                 <div className="homecard-container">
                     <a href="" className="sell-btn">
                         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -196,135 +151,9 @@ export default function Homemain(){
                         </svg>
                         Jual
                     </a>
-                    <div className="homegrid-container">
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <img src={Clockone} className="item-img" alt="Clock" />
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <img src={Clocktwo} className="item-img" alt="Clock"/>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <div className="item-img"></div>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>  
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <div className="item-img"></div>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <div className="item-img"></div>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <div className="item-img"></div>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>  
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <div className="item-img"></div>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <div className="item-img"></div>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <div className="item-img"></div>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>  
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <div className="item-img"></div>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>  
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <div className="item-img"></div>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>  
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <div className="item-img"></div>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <div className="item-img"></div>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>  
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <div className="item-img"></div>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>  
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <div className="item-img"></div>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>  
-                        <div className="homegrid-item">
-                            <div className="card-item">
-                                <div className="item-img"></div>
-                                <h5>Jam Tangan Casio</h5>
-                                <h6>Aksesoris</h6>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div> 
+                    
+                    <div className="homegrid-container">                        
+                        <Homecard />
                     </div>
                 </div>
             </div>
