a8eafc2f748788628b780d2aea0d7ada7eb89ff0
wid46do <rubenemmanuel2000w@gmail.com>
2022-07-24 23:05:22 +0700

    refactor: refactor product name

--- src/components/DataProductSale.jsx
+++ src/components/DataProductSale.jsx
@@ -5,6 +5,7 @@ import { FiPlus } from "react-icons/fi";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import "../style/DataProductSale.css";
+import LinesEllipsis from 'react-lines-ellipsis'
 
 function DataProductSale() {
   const [produk, setProduk] = useState([]);
@@ -55,7 +56,12 @@ function DataProductSale() {
                 />
               </div>
               <div className="h-20">
-                <h5 className="font-size-14 mb-1">{produk.product_name}</h5>
+                {/* <h5 className="font-size-14 mb-1">{produk.product_name}</h5> */}
+                <LinesEllipsis
+                  text={produk.product_name}
+                  length={16}
+                  ellipsis='...'
+                />
                 <p className="font-size-10 color-gray mb-2">Aksesoris</p>
                 <p className="font-size-14 ">{produk.product_harga}</p>
               </div>
